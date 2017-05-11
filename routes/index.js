3db6222e450de767c5262641e2bbdbd6318630a4
Shailesh Yedge <shaileshyedge@Shaileshs-MacBook-Pro.local>
2017-05-11 08:33:19 -0700

    data

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var mongourl = "mongodb://localhost:27017/PotholeData";
+var MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
